d73f43dae5d2bbe16e7b1cc0ce776095623f5d53
jeanpauldione <dionepauljean@gmail.com>
2019-10-01 22:19:16 +0000

    creation de la page d'anthentification

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+    <div class="login">
+            <center>
+                <!--<h1>BIENVENUE SUR LA PLATEFORME DE GESTION DES APPRENANTS</h1>-->
+                <h1>BIENVENUE SUR LA PLATEFORME DE GESTION DES R-V</h1>
+                <form method="POST"  action="authentifier.php">
+                    <p>Merci de vous Authentifier</p>
+                    <input type="text" name="username" placeholder="login" required> <br>
+                    <input type="password" name="password" placeholder="password" required> <br>  
+                    <button type="submit">Se Connecter</button>
+
+                </form>
+            </center>   
+        </div>
+    </body>
+</html>
